963d438c692e8ddfba47dcdfdfa6876e3f652b16
welentsai <welentsai@gmail.com>
2024-07-06 07:57:27 +0800

    feat: init projects

---
+++ dop/src/main/java/com/example/dop/DopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopApplication.class, args);
+	}
+
+}
