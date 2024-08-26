33be7c69b0de6d5dcc483f691954ad4db99e99c6
welentsai <welentsai@gmail.com>
2024-08-26 21:51:14 +0800

    feat: move the road to react independent folder

---
+++ theroadtoreact/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:8080",
+                changeOrigin: true,
+                secure: false,
+                rewrite: function (path) { return path.replace(/^\/api/, ""); },
+            }
+        }
+    },
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './tests/setup.ts',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            exclude: ['node_modules']
+        }
+    },
+});
