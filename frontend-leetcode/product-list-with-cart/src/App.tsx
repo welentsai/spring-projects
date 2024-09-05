d02ebc7396039de92ad91fc179cf6c99522238db
welentsai <welentsai@gmail.com>
2024-09-05 22:43:16 +0800

    feat: product-list leetcode

--- frontend-leetcode/product-list-with-cart/src/App.tsx
+++ frontend-leetcode/product-list-with-cart/src/App.tsx
@@ -1,37 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import desserts from './data.json'
+import { Dessert, DessertCard } from './components/DessertCard'
+import { ShoppingCard } from './components/ShoppingCard'
+import { OrderProvider } from './context/OrderContext'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <OrderProvider>
+      <div className='flex flex-col sm:flex-row gap-2'>
+        <div className='sm:w-3/4'>
+          <h1 className="text-3xl font-bold text-left pb-5">
+            Desserts
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {desserts.map((dessert: Dessert, index: number) => (
+              <DessertCard
+                dessert={dessert} key={index} />
+            ))}
+          </div>
+        </div>
+        <div className='sm:w-1/4'>
+          <ShoppingCard></ShoppingCard>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </OrderProvider>
   )
 }
 
