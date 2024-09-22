73835466eaf51eeb267af875e1140c9fb3d2379f
welentsai <welentsai@gmail.com>
2024-09-22 16:18:29 +0800

    refactor the App.tsc layout

--- frontend-leetcode/mortage-repayment-calculator/src/App.tsx
+++ frontend-leetcode/mortage-repayment-calculator/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import { CurrencyInput } from './components/CurrencyInput'
+import { CenterLayout } from './layout/CenterLayout/component';
+import { TwoColumnLayout } from './layout/TwoColumnLayout';
 
 function App() {
   const [amount, setAmount] = useState(500)
@@ -12,24 +14,46 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-sky-200 flex items-center justify-center">
-        <div className='grid grid-cols-2'>
+      <div className="flex justify-center items-center min-h-screen bg-blue-50">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden max-w-4xl w-full">
+          <div className="flex">
+            {/* Left column */}
+            <div className="w-1/2 p-4 bg-gray-50">
+              <div className='flex justify-between'>
+                <h2 className='text-xl'>Mortgage Calculator</h2>
+                <p className='p-2'>Clear all</p>
+              </div>
+              <div className='bg-gray-50'>
+                <p>Mortgage Account</p>
+                <CurrencyInput amount={amount} handleAmountChange={handleAmountChange} />
+              </div>
+
+            </div>
+            {/* Right column */}
+            <div className="w-1/2 bg-blue-800 p-4 text-white">
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <CenterLayout>
+        <TwoColumnLayout>
           <div>
-            <div className='flex w-96 h-32 bg-gray-50'>
+            <div className='  bg-gray-50'>
               <h2 className='p-4 text-xl'>Mortgage Calculator</h2>
               <p className='p-4'>Clear all</p>
-            </div>
-            <div>
+            </div>  
+            <div className='bg-gray-50'>
               <p>Mortgage Account</p>
               <CurrencyInput amount={amount} handleAmountChange={handleAmountChange} />
             </div>
           </div>
-
-          <div className='w-96 h-32 bg-blue-800'>
-            <h2 className='p-4 text-white text-base'>Your results</h2>
+          <div className='w-96'>
+            <div className='bg-blue-800'>
+              <h2 className='p-4 text-white text-base'>Your results</h2>
+            </div>
           </div>
-        </div>
-      </div>
+        </TwoColumnLayout >
+      </CenterLayout > */}
     </>
   )
 }
