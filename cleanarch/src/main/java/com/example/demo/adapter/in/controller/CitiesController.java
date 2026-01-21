329ceccf2a295625ac68f72d972dc5e21963d079
welentsai <welentsai@gmail.com>
2026-01-21 23:26:10 +0800

    chore: adopt openrewrite

--- cleanarch/src/main/java/com/example/demo/adapter/in/controller/CitiesController.java
+++ cleanarch/src/main/java/com/example/demo/adapter/in/controller/CitiesController.java
@@ -6,7 +6,9 @@ import com.example.demo.usecase.ports.in.FindCitiesUseCase;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/cities")
